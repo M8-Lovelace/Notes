ab57040784620bc8061d2b08b6ade8e02786f4b9
J6-Lovelace <lauritasusano.10@gmail.com>
2023-01-03 12:32:00 -0500

    chore: update overload

--- src/16-overload.ts
+++ src/16-overload.ts
@@ -1,28 +1,52 @@
 // Laura => [L,a,u,r,a] => string => string[]
 // [L,a,u,r,a] => Laura => string[] => string
 
-function parseStr(input: string | string[]): string | string[] {
+// ----------------------------Problema--------------------------------//
+// Recibe como parametros un string o un array con string
+function parseStr1(input: string | string[]): string | string[] {
+  if (Array.isArray(input)) {
+    return input.join(""); // string
+  } else {
+    return input.split(""); // string[]
+  }
+}
+
+const rtaArray2 = parseStr('Laura');
+// rtaArray.reverse();
+if (Array.isArray(rtaArray2)) {
+  rtaArray2.reverse();
+}
+console.log('1. rtaArray reverse', 'Laura =>' ,rtaArray2); // [ 'a', 'r', 'u', 'a', 'L' ]
+
+// Con el 'as' define el tipo de dato a retornar
+const rtaArray3 = parseStr1("Laura") as string[];
+console.log("1. rtaArray", "Laura =>", rtaArray3); // [ 'L', 'a', 'u', 'r', 'a' ]
+
+
+// ----------------------------Solución--------------------------------//
+// Predefiniendo el tipo de datos a retornar
+export function parseStr(input: string): string[];
+export function parseStr(input: string[]): string;
+export function parseStr(input: number): boolean;
+
+// Buenas practicas, si hay un any o un unkown en el overload se deja al final
+export function parseStr(input: unknown): unknown {
   if (Array.isArray(input)) {
     return input.join(''); // string
-  } else {
+  } else if (typeof input === 'string'){
     return input.split(''); // string[]
+  } else if (typeof input === 'number'){
+    return true; // boolean
   }
 }
 
 const rtaArray = parseStr('Laura');
-console.log('rtaArray', 'Laura =>', rtaArray); // [ 'L', 'a', 'u', 'r', 'a' ]
+rtaArray.reverse();
+console.log('2. rtaArray reverse', 'Laura =>' ,rtaArray); // [ 'a', 'r', 'u', 'a', 'L' ]
 
-// --------------------------------------------------------------------//
-// rtaArray.reverse();
-if (Array.isArray(rtaArray)) {
-  rtaArray.reverse();
-}
-console.log('rtaArray reverse', 'Laura =>' ,rtaArray); // [ 'a', 'r', 'u', 'a', 'L' ]
-
-// --------------------------------------------------------------------//
 const rtaStr = parseStr(['L','a','u','r','a']);
-// rtaStr.toLowerCase();
-if (typeof rtaStr === 'string') {
-  rtaStr.toLowerCase();
-}
-console.log('rtaStr', "['L','a','u','r','a'] =>",rtaStr); // Laura
+rtaStr.toLowerCase();
+console.log('2. rtaStr', "['L','a','u','r','a'] =>",rtaStr); // Laura
+
+const rtaBoolean = parseStr(12);
+console.log('2. rtaBoolean =>',rtaBoolean);
