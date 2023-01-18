13359c3692e6549d9476fdeaf7a6faf324fc9d50
J6-Lovelace <lauritasusano.10@gmail.com>
2023-01-18 12:21:03 -0500

    chore: add decorators

--- TypeScript/src/class/app/models/category.model.ts
+++ TypeScript/src/class/app/models/category.model.ts
@@ -1,5 +1,11 @@
+export enum AccessType {
+  PRIVATE = 'private',
+  PUBLIC = 'public'
+}
+
 export interface Category {
   id:    number;
   name:  string;
   image: string;
+  access?: AccessType;
 }
