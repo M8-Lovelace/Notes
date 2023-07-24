bf7123a809060640492a6fa6d1ab9747853bf17a
Laura Susano <lauritasusano.10@gmail.com>
2023-07-24 16:15:24 -0500

    feat(java): add new operator notes

--- Java/Fundamentals/src/Operators.java
+++ Java/Fundamentals/src/Operators.java
@@ -1,9 +1,14 @@
+
 // Importamos la libreria de JOptionPane
-import javax.swing.*;
 
 // Un operador lleva a cabo operaciones sobre uno (operador unario), dos
 // (operador binario) o tres (operador ternario) datos u operandos de tipo
 // primitvo devolviendo un valor determinado también de un tipo primitivo.
+
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
 public class Operators {
     // --------------------------------------------------------------------//
     // --------------------- OPERADORES ARITMÉTICOS -----------------------//
@@ -44,7 +49,7 @@ public class Operators {
         int numero = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un numero"));
 
         // Evalua si el número es par o impar
-        if(numero % 2 == 0){
+        if (numero % 2 == 0) {
             System.out.println("numero par = " + numero);
         } else {
             System.out.println("numero impar = " + numero);
@@ -135,6 +140,185 @@ public class Operators {
         System.out.println("++c = " + (++c)); // 5
         System.out.println("c++ = " + (c++)); // 5
         System.out.println("c = " + c); // 6
+        System.out.println(" ");
+
+        // --------------------------------------------------------------------//
+        // --------------------- OPERADORES RELACIONALES ----------------------//
+        // Son los operadores que se utilizan para comparar dos valores. Los
+        // operadores relacionales son: >, <, >=, <=, ==, !=
+
+        int i1 = 3;
+        byte j1 = 7;
+        float k1 = 127e-7f;
+        double l1 = 2.1413e3;
+        boolean m1 = false;
+
+        boolean b1 = i1 == j1;
+        System.out.println("b1 = " + b1); // false
+
+        boolean b2 = !b1;
+        System.out.println("b2 = " + b2); // true
+
+        boolean b3 = i1 != j; // <>
+        System.out.println("b3 = " + b3); // true
+
+        boolean b4 = m1 == true;
+        System.out.println("b4 = " + b4); // false
+
+        boolean b5 = m1 != true;
+        System.out.println("b5 = " + b5); // true
+
+        boolean b6 = i1 > j1;
+        System.out.println("b6 = " + b6); // false
+
+        boolean b7 = i1 < j1;
+        System.out.println("b7 = " + b7); // true
+
+        boolean b8 = l1 >= k1;
+        System.out.println("b8 = " + b8); // true
+        System.out.println(" ");
+
+        // --------------------------------------------------------------------//
+        // --------------------- OPERADORES LÓGICOS ---------------------------//
+        // AND (y), OR (o), NOT (no)
+
+        boolean b9 = true && true;
+        System.out.println("b9 = " + b9); // true
+
+        boolean b10 = i1 >= j1 && m1 == true;
+        System.out.println("b10 = " + b10); // false
+
+        boolean b11 = i1 >= j1 || m1 == true;
+        System.out.println("b11 = " + b11); // false
+
+        boolean b12 = i1 >= j1 && m1 != true || l1 >= k1;
+        System.out.println("b12 = " + b12); // true
+
+        boolean b13 = !(i1 >= j1 && m1 != true || l1 >= k1);
+        System.out.println("b13 = " + b13); // false
+
+        // EJEMPLO LOGIN
+        String[] usernames = { "edwin", "laura" };
+        String[] passwords = { "12345", "54321" };
+
+        // Definir un Scanner para poder leer valores del teclado
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese el username: ");
+
+        // Leemos el valor que el usuario ingresa por consola
+        String u = scanner.next();
+
+        System.out.print("Ingrese el password: ");
+
+        // Leemos el valor que el usuario ingresa por consola
+        String p = scanner.next();
+
+        // Si la condición es verdadera, esAutenticado es verdadero, sino es falso
+        boolean esAutenticado = false;
+
+        // Iteramos el arreglo para verificar si existe el username y password
+        for (int ii = 0; ii < usernames.length; ii++) {
+            esAutenticado = usernames[ii].equals(u) && passwords[ii].equals(p) ? true : esAutenticado;
+
+            // if (usernames[ii].equals(u) && passwords[ii].equals(p)) {
+            // esAutenticado = true;
+            // break;
+            // }
+        }
+
+        // Verificamos si esAutenticado es verdadero o falso
+        String mensaje = esAutenticado ? "Bienvenido usuario ".concat(u).concat("!")
+                : "Username o password incorrecto!";
+
+        System.out.println("mensaje = " + mensaje);
+
+        // Cerramos el Scanner
+        scanner.close();
+        System.out.println(" ");
+
+        // --------------------------------------------------------------------//
+        // --------------------- OPERADORES TERNARIOS -------------------------//
+
+        // variable = condicion ? true: false;
+        String variable = 7 == 5 ? "true" : "false";
+        System.out.println("variable = " + variable); // false
+
+        String estado = "";
+        double promedio = 0.0;
+
+        double matematicas = 0.0;
+        double ciencias = 0.0;
+        double historia = 0.0;
+
+        matematicas = 5.5;
+        ciencias = 6.0;
+        historia = 4.5;
+
+        promedio = (matematicas + ciencias + historia) / 3;
+
+        estado = promedio >= 5.49 ? "Aprobado" : "Desaprobado";
+        System.out.println("El promedio fue " + promedio + ", el estado es = " + estado); // Desaprobado
+
+        // Operador ternario número mayor
+
+        int max = 0;
+
+        int num1 = 5;
+        int num2 = 10;
+        int num3 = 15;
+
+        max = num1 > num2 ? num1 : num2;
+        max = max > num3 ? max : num3;
+
+        System.out.println("El número mayor es = " + max);
+        System.out.println(" ");
+
+        // --------------------------------------------------------------------//
+        // --------------------- OPERADORES DE INSTANCIA ----------------------//
+        // Son los operadores que se utilizan para saber si un objeto es de un
+        // tipo determinado. Los operadores de instancia son: instanceof
+
+        String texto = "Creando un objeto de la clase String...";
+        Integer num = 7;
+
+        boolean c1 = texto instanceof String;
+        System.out.println("texto es del tipo String = " + c1); // true
+
+        c1 = texto instanceof Object;
+        System.out.println("num es del tipo Object = " + c1); // true
+
+        c1 = num instanceof Integer;
+        System.out.println("num es del tipo Integer = " + c1); // true
+        System.out.println(" ");
+
+        // Tipos abstractos
+        Object fecha = new java.util.Date();
+        Boolean boolea = false;
+        // c1 = fecha instanceof Date; // Clase padre no implementada en Java SE
+        System.out.println("fecha es del tipo Date = " + c1); // true
+        c1 = fecha instanceof Object;
+        System.out.println("fecha es del tipo Object = " + c1); // true
+        c1 = boolea instanceof Boolean;
+        System.out.println("boolea es del tipo Boolean = " + c1); // true
+        c1 = boolea instanceof Object;
+        System.out.println("boolea es del tipo Object = " + c1); // true
+        System.out.println(" ");
+
+        // --------------------------------------------------------------------//
+        // ------------------- PRECEDENCIA DE OPERADORES ----------------------//
+        // La precedencia de operadores es la siguiente:
+        // 1. ()
+        // 2. ++, -- (antes de la variable)
+        // 3. ^
+        // 4. *, /, %
+        // 5. +, -
+        // 6. <, <=, >, >=
+        // 7. ==, !=
+        // 8. &&
+        // 9. ||
+        // 10. =, +=, -=, *=, /=, %=
+        // 11. ++, -- (después de la variable)
+
     }
 }
-
--- Java/Fundamentals/src/Strings.java
+++ Java/Fundamentals/src/Strings.java
@@ -4,7 +4,8 @@ public class Strings {
     String nombre = new String("Laura");
     String nombre2 = "Laura";
 
-    // El tipo String tiene una característica especial, permite crear objetos también
+    // El tipo String tiene una característica especial, permite crear objetos
+    // también
     // en la literal entre comillas dobles.
     // Para incluir el carácter comillas dobles, se debe escapar "\"".
     String tema = new String("Manejo de \"String\"");
@@ -21,25 +22,30 @@ public class Strings {
     int numeroA = 10;
     int numeroB = 5;
 
-    // Al poner al inicio un string y luego unas variables numéricas estos se van a concatenar.
+    // Al poner al inicio un string y luego unas variables numéricas estos se van a
+    // concatenar.
     System.out.println(detalle + numeroA + numeroB); // Programación Java con el instructor Edwin Páez105
-    // Si probamos poner primero los números estos se van a sumar y luego concatenar al string.
+    // Si probamos poner primero los números estos se van a sumar y luego concatenar
+    // al string.
     System.out.println(numeroA + numeroB + detalle); // 15Programación Java con el instructor Edwin Páez
-    // La solución para hacer una suma y luego concatenar es encerrando los números en paréntesis.
-    System.out.println(detalle + (numeroA + numeroB )); // Programación Java con el instructor Edwin Páez15
+    // La solución para hacer una suma y luego concatenar es encerrando los números
+    // en paréntesis.
+    System.out.println(detalle + (numeroA + numeroB)); // Programación Java con el instructor Edwin Páez15
 
     // Aquí probaremos utilizando el método concat, el cual es más eficiente
     String detalle2 = curso.concat(" con ").concat(profesor);
     System.out.println("detalle2 = " + detalle2); // Programación Java con Edwin Páez
 
-    // StringBuilder es una clase que permite crear un objeto mutable de tipo String.
+    // StringBuilder es una clase que permite crear un objeto mutable de tipo
+    // String.
     StringBuilder sb = new StringBuilder(curso);
     // Tomar el tiempo de inicio
     long inicio = System.currentTimeMillis();
     // Invocar el método append() de la clase StringBuilder para concatenar
     sb.append(" con ").append(profesor);
-    //  Método que ejecuta el garbage collector del sistema. Realiza un reciclado de los objetos de la memoria
-    //  de la JVM para optimizar el espacio de la misma.
+    // Método que ejecuta el garbage collector del sistema. Realiza un reciclado de
+    // los objetos de la memoria
+    // de la JVM para optimizar el espacio de la misma.
     System.gc();
     // Tomar el tiempo de fin
     long fin = System.currentTimeMillis();
@@ -55,25 +61,26 @@ public class Strings {
     String profesor2 = "Laura Susano";
 
     String resultado = curso2.concat(profesor2);
-    System.out.println("curso2 = " + curso2);  // Programación Java
-    System.out.println("resultado = " + resultado);  // Programación JavaLaura Susano
-    System.out.println((curso2 == resultado));  // false
+    System.out.println("curso2 = " + curso2); // Programación Java
+    System.out.println("resultado = " + resultado); // Programación JavaLaura Susano
+    System.out.println((curso2 == resultado)); // false
 
     // El método transform() permite modificar el String y devolver el resultado.
-    String resultado2 = curso2.transform(c ->{
+    String resultado2 = curso2.transform(c -> {
       return c + " con " + profesor2;
     });
     System.out.println("curso2 = " + curso2); // Programación Java
-    System.out.println("resultado2 = " + resultado2);  // Programación Java con Laura Susano
+    System.out.println("resultado2 = " + resultado2); // Programación Java con Laura Susano
 
     // El método replace() permite modificar el String y devolver el resultado.
-    // El primer argumento es el caracter a buscar y el segundo el caracter a reemplazar.
+    // El primer argumento es el caracter a buscar y el segundo el caracter a
+    // reemplazar.
     // Si no encuentra el caracter a buscar, no hace nada.
-    String resultado3 = curso2.replace("a","A");  // Programción JAvA
+    String resultado3 = curso2.replace("a", "A"); // Programción JAvA
     // Este método no modifica el String original.
-    System.out.println("curso2 = " + curso2);  // Programación Java
+    System.out.println("curso2 = " + curso2); // Programación Java
     // Guarda el resultado en una nueva variable.
-    System.out.println("resultado3 = " + resultado3);  // Programción JAvA
+    System.out.println("resultado3 = " + resultado3); // Programción JAvA
     System.out.println(" ");
 
     // --------------------------------------------------------------------//
@@ -90,7 +97,9 @@ public class Strings {
     System.out.println("Tienen el mismo valor? " + str1.equals(str2)); // true
     // Chequea si tienen el mismo valor, sin importar mayúsculas o minúsculas
     System.out.println("Tienen el mismo valor? " + str1.equalsIgnoreCase(str2)); // true
-    // Compara orden numérico de las cadenas basado en el valor Unicode de cada carácter, si es menor a 0 es menor, si es mayor a 0 es mayor, si es igual a 0 es igual
+    // Compara orden numérico de las cadenas basado en el valor Unicode de cada
+    // carácter, si es menor a 0 es menor, si es mayor a 0 es mayor, si es igual a 0
+    // es igual
     System.out.println("str1.compareTo(\"Laura\") = " + str1.compareTo("Laura")); // -4
     System.out.println("str1.compareTo(\"Aaron\") = " + str1.compareTo("Aaron")); // 7
 
@@ -108,7 +117,7 @@ public class Strings {
     boolean isNull = curso3 == null;
     System.out.println("isNull = " + isNull); // true
 
-    if (isNull){
+    if (isNull) {
       curso3 = "Programación Java";
     }
 
@@ -120,12 +129,13 @@ public class Strings {
     boolean esVacio2 = curso3.isEmpty();
     System.out.println("esVacio2 = " + esVacio2); // false
 
-    // Verifica si la cadena está vacía, valida la longitud y si tiene espacios en blanco
+    // Verifica si la cadena está vacía, valida la longitud y si tiene espacios en
+    // blanco
     boolean isWhite = curso3.isBlank();
     System.out.println("isWhite = " + isWhite); // false
 
     // Verifica si la cadena no está vacía
-    if (!isWhite){
+    if (!isWhite) {
       // Convierte la cadena a mayúsculas
       System.out.println(curso3.toUpperCase()); // PROGRAMACIÓN JAVA
       // Concatena la cadena
@@ -135,10 +145,13 @@ public class Strings {
 
     // --------------------------------------------------------------------//
     // -----------------------------MÉTODOS--------------------------------//
-    // Los métodos de la clase String son inmutables, no modifican el String original.
+    // Los métodos de la clase String son inmutables, no modifican el String
+    // original.
     // Devuelven un nuevo String con el resultado.
-    // Los métodos de la clase String son estáticos, se pueden invocar sin crear un objeto.
-    // Los métodos de la clase String son públicos, se pueden invocar desde cualquier clase.
+    // Los métodos de la clase String son estáticos, se pueden invocar sin crear un
+    // objeto.
+    // Los métodos de la clase String son públicos, se pueden invocar desde
+    // cualquier clase.
     String nombre3 = "Laura";
 
     // Cuenta la cantidad de caracteres de la cadena
@@ -155,29 +168,38 @@ public class Strings {
 
     // Obtiene una subcadena de la cadena, desde la posición indicada hasta el final
     System.out.println("nombre3.substring(1) = " + nombre3.substring(1)); // aura
-    // Obtiene una subcadena de la cadena, desde la posición indicada hasta la posición indicada
+    // Obtiene una subcadena de la cadena, desde la posición indicada hasta la
+    // posición indicada
     System.out.println("nombre3.substring(1, 4) = " + nombre3.substring(1, 4)); // aur
-    // Obtiene una subcadena de la cadena, desde la posición indicada hasta la posición indicada, pero de forma inversa
+    // Obtiene una subcadena de la cadena, desde la posición indicada hasta la
+    // posición indicada, pero de forma inversa
     System.out.println("nombre3.substring(4, 6) = " + nombre3.substring(nombre.length() - 2)); // ra
 
     String trabalenguas = "trabalenguas";
 
-    // Reemplaza todas las ocurrencias de la cadena que se pasa como primer argumento, por la cadena que se pasa como segundo argumento
+    // Reemplaza todas las ocurrencias de la cadena que se pasa como primer
+    // argumento, por la cadena que se pasa como segundo argumento
     System.out.println("trabalenguas.replace(\"a\",\".\") = " + trabalenguas.replace("a", ".")); // tr.b.lengu.s
     System.out.println("trabalenguas = " + trabalenguas); // trabalenguas
-    // Nos da la posición de la primera ocurrencia de la cadena que se pasa como argumento
+    // Nos da la posición de la primera ocurrencia de la cadena que se pasa como
+    // argumento
     System.out.println("trabalenguas.indexOf(\"a\") = " + trabalenguas.indexOf("a")); // 1
-    // Nos da la posición de la última ocurrencia de la cadena que se pasa como argumento
+    // Nos da la posición de la última ocurrencia de la cadena que se pasa como
+    // argumento
     System.out.println("trabalenguas.lastIndexOf(\"a\") = " + trabalenguas.lastIndexOf("a")); // 9
-    // Nos dice si la cadena que se pasa como argumento está contenida en la cadena, si da un valor mayor a -1 es que está contenida
+    // Nos dice si la cadena que se pasa como argumento está contenida en la cadena,
+    // si da un valor mayor a -1 es que está contenida
     System.out.println("trabalenguas.indexOf(\"t\") = " + trabalenguas.indexOf("t")); // 0
-    // Nos dice si está contenida la cadena que se pasa como argumento en la cadena, si da un valor true es que está contenida
+    // Nos dice si está contenida la cadena que se pasa como argumento en la cadena,
+    // si da un valor true es que está contenida
     System.out.println("trabalenguas.contains(\"t\") = " + trabalenguas.contains("t")); // true
-    // Nos dice si la cadena que se pasa como argumento empieza con la cadena, si da un valor true es que empieza con la cadena
+    // Nos dice si la cadena que se pasa como argumento empieza con la cadena, si da
+    // un valor true es que empieza con la cadena
     System.out.println("trabalenguas.startsWith(\"tra\") = " + trabalenguas.startsWith("tra")); // true
-    // Nos dice si la cadena que se pasa como argumento termina con la cadena, si da un valor true es que termina con la cadena
+    // Nos dice si la cadena que se pasa como argumento termina con la cadena, si da
+    // un valor true es que termina con la cadena
     System.out.println("trabalenguas.endsWith(\"lenguas\") = " + trabalenguas.endsWith("lenguas")); // true
-    System.out.println("   " + "trabalenguas "); // "   trabalenguas "
+    System.out.println("   " + "trabalenguas "); // " trabalenguas "
     // Elimina los espacios en blanco al inicio y al final de la cadena
     System.out.println(trabalenguas.trim()); // trabalenguas
     System.out.println(" ");
@@ -210,20 +232,23 @@ public class Strings {
       System.out.print(arreglo[o]); // trabalenguas
     }
 
-    // Convierte la cadena a un arreglo de caracteres, separando la cadena por el caracter que se pasa como argumento
+    // Convierte la cadena a un arreglo de caracteres, separando la cadena por el
+    // caracter que se pasa como argumento
     System.out.println("\ntrabalenguas2 = " + trabalenguas2.split("a")); // [Ljava.lang.String;@4554617c
-    // Divide la cadena en un arreglo de cadenas, separando la cadena por el caracter que se pasa como argumento
+    // Divide la cadena en un arreglo de cadenas, separando la cadena por el
+    // caracter que se pasa como argumento
     String[] arreglo2 = trabalenguas2.split("a");
     int l = arreglo2.length;
     // Recorre el arreglo
-    for (int j=0; j < l; j++){
+    for (int j = 0; j < l; j++) {
       // Imprime el arreglo
       System.out.println(arreglo2[j]); // tr, b , lengu, s
     }
 
     String archivo2 = "alguna.imagen.pdf";
-    // Convierte la cadena a un arreglo de caracteres, separando la cadena por el caracter que se pasa como argumento
-    String[] archivoArr = archivo2.split("\\."); //[.]
+    // Convierte la cadena a un arreglo de caracteres, separando la cadena por el
+    // caracter que se pasa como argumento
+    String[] archivoArr = archivo2.split("\\."); // [.]
     // Obtiene el largo del arreglo
     int largoArchivoArr = archivoArr.length;
 
